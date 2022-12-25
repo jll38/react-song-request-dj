cc06d8cf698b51b2df28bf60105c4ab5a970d688
Julian <jll38@njit.edu>
2022-12-25 00:59:38 -0500

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,22 +1,27 @@
 import logo from './logo.svg';
 import './App.css';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import Form from 'react-bootstrap/Form';
+import Button from 'react-bootstrap';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+       <div id="PSK-HEAD">
+        <img src='https://cdn.shopify.com/s/files/1/0175/2324/5110/products/phi-sigma-kappa-main-logo-sticker_300x300.png?v=1610773892'></img>
+        <h1>Phi Sigma Kappa</h1>
+        <h2>Song Requests</h2>
+       </div>
+       <div id='songbox'>
+        <Form>
+          <Form.Control type='text' placeholder="Song Name" id='songInput'/>
+          <Form.Control type='text' placeholder="Artist" id='artistInput'/>
+        </Form>
+        <div id='queue'>
+          test
+        </div>
+       </div>
       </header>
     </div>
   );
